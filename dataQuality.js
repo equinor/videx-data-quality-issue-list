95f3b54a5966bbb756d83c8855d6546a47b9b36c
Brynjulf Risbakken <brisb@equinor.com>
2021-11-05 09:04:15 +0100

    removed obsolete function

--- dataQuality.js
+++ dataQuality.js
@@ -1,29 +1,3 @@
-/**
- *
- * @param {*} issue - issue object without injected attributes
- * @param {*} options - attributes to be injected in the message
- * @returns transformed data issue
- */
-export function transformIssue(issue, options = {}) {
-  const attributes = Object.keys(options);
-
-  let message = issue?.message;
-  let resourceUuid = issue?.resourceUuid;
-  let shortDescription = issue?.shortDescription;
-  attributes.map(attribute => {
-    message = message?.replace(`\${${attribute}}`, options[attribute]);
-    shortDescription = shortDescription?.replace(`\${${attribute}}`, options[attribute]);
-    resourceUuid = resourceUuid?.replace(`${attribute}`, options[attribute]);
-  });
-
-  return {
-    ...issue,
-    message,
-    resourceUuid,
-    shortDescription,
-  };
-}
-
 export const HEADER_NOT_FOUND = (wellboreUuid, wellboreName) => ({
   code: HEADER_NOT_FOUND,
   category: 'Wellbore',
